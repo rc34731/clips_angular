762a1dd8e56d967750cc050595cca3c7fa000715
rc34731 <54817704+rc34731@users.noreply.github.com>
2023-10-14 16:34:36 -0400

    Add files via upload

---
+++ manage.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { Params, Router, ActivatedRoute } from '@angular/router';
+import IClip from 'src/app/models/clip.model';
+import { ClipService } from 'src/app/services/clip.service';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-manage',
+  templateUrl: './manage.component.html',
+  styleUrls: ['./manage.component.css']
+})
+export class ManageComponent implements OnInit {
+  videoOrder = '1'
+  clips: IClip[] = []
+  activeClip: IClip | null = null
+
+  constructor(
+    private router: Router, 
+    private route: ActivatedRoute,
+    private clipService: ClipService,
+    private modal: ModalService
+    ){}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.videoOrder = params['sort'] === '2' ? params['sort']: 1;
+    })
+    this.clipService.getUserClips().subscribe(docs => {
+      this.clips = []
+
+      docs.forEach(doc => {
+        this.clips.push({
+          docID: doc.id,
+          ...doc.data()
+        })
+      })
+    })
+  }
+
+  sort(event: Event){
+    const { value } = (event.target as HTMLSelectElement)
+
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        sort: value
+      }
+    })
+  }
+
+  openModal($event: Event, clip: IClip) {
+    $event.preventDefault()
+
+    this.activeClip = clip
+
+    this.modal.toggleModal('editClip')
+  }
+
+  update($event: IClip){
+    this.clips.forEach((element, index) => {
+      if(element.docID == $event.docID){
+        this.clips[index].title = $event.title
+      }
+    })
+  }
+
+  deleteClip($event: Event, clip: IClip){
+    $event.preventDefault()
+
+    this.clipService.deleteClip(clip)
+
+    this.clips.forEach((element, index) => {
+      if(element.docID == clip.docID){
+        this.clips.splice(index, 1)
+      }
+    })
+  }
+}
