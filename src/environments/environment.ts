f93404fe2f887edf41524ea153c3962ef437e8ea
Richard <rc34731@umbc.edu>
2024-01-05 12:11:51 -0500

    All completed project

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyAEOa__6cqaB7dbfA7nAEODdA3N9VwB5Wk",
+        authDomain: "clips-8344c.firebaseapp.com",
+        projectId: "clips-8344c",
+        storageBucket: "clips-8344c.appspot.com",
+        appId: "1:308047467089:web:a35549916db9fcec0fde70"
+      }
+};
